0f78f4e0431a09436ee18beebc27807eece0098d
patleserdev <leserpat@hotmail.com>
2024-06-28 11:23:36 +0200

    update routes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/ear', indexRouter);
+app.use('/', indexRouter);
 
 
 module.exports = app;
